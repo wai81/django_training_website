84b044bcfbf5766c20cdfabf38c668ca86c39a03
wai81 <50743966+wai81@users.noreply.github.com>
2021-06-30 01:14:42 +0300

    create app CMS

--- landingpagewebsite/settings.py
+++ landingpagewebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cms.apps.CmsConfig',
     'crm.apps.CrmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,8 +127,8 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_URL = '/static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+ # MEDIA_URL = '/media/'
+ # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
