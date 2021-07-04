6034ba7943aafc203803309451222437ccad29ce
wai81 <50743966+wai81@users.noreply.github.com>
2021-07-04 22:24:43 +0300

    add exeption

--- telebot/sendmessage.py
+++ telebot/sendmessage.py
@@ -3,22 +3,38 @@ from .models import TeleBotSettings
 
 
 def sendTelegram(tg_name, tg_phone):
-    settings = TeleBotSettings.objects.get(pk=1)
+    if TeleBotSettings.objects.get(pk=1):
+        settings = TeleBotSettings.objects.get(pk=1)
+        token = str(settings.tg_token)
+        chat_id = str(settings.tg_chat)
+        text = str(settings.tg_message)
 
-    token = str(settings.tg_token)
-    chat_id = str(settings.tg_chat)
-    text = str(settings.tg_message)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendMessage'
 
-    api = 'https://api.telegram.org/bot'
-    method = api + token + '/sendMessage'
+        if text.find('{') and text.find('}') and text.rfind('{') and text.rfind('}'):
+            part_1 = text[0:text.find('{')]
+            part_2 = text[text.find('}') + 1:text.rfind('{')]
+            part_3 = text[text.rfind('}'):-1]
+            text_slise = part_1 + tg_name + part_2 + tg_phone + part_3
+        else:
+            text_slise = text
 
-    part_1 = text[0:text.find('{')]
-    part_2 = text[text.find('}') + 1:text.rfind('{')]
-    part_3 = text[text.rfind('}'):-1]
+        try:
+            req = requests.post(method, data={
+                'chat_id': chat_id,
+                'text': text_slise
+            })
+        except:
+            pass
 
-    text_slise = part_1 + tg_name + part_2 + tg_phone + part_3
+        finally:
+            if req.status_code != 200:
+                print('Ошибка отправки!')
+            elif req.status_code == 500:
+                print('Ошибка 500')
+            else:
+                print('Все Ок сообщение отбравлено!')
 
-    req = requests.post(method, data={
-        'chat_id': chat_id,
-        'text': text_slise
-    })
+    else:
+        pass
